a276f3b95fb5f3f2d668f0665c202567771a9d33
slavapas <pascaruv@gmail.com>
2017-10-12 08:31:28 +0300

    change the gulpfile.js for the gp (gulp-load-plugins) and stylus

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,20 @@
 var gulp = require ('gulp'),
     gp   = require ('gulp-load-plugins')();
 
+
+// add pug
 gulp.task('pug', function(){
     return gulp.src('src/pug/pages/*.pug')
         .pipe(gp.pug({
             pretty: true
         }))
+        .pipe(gulp.dest('build'));
+});
+
+// add stylus
+gulp.task('stylus', function(){
+    return gulp.src('src/static/stylus/main.styl')
+    // here you can add options from web site of this plugin between curly brackets (stylus)
+        .pipe(gp.stylus({}))
         .pipe(gulp.dest('build'));
 });
